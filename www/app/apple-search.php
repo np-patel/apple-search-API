95cce30c1edc3d69a9b2901d6f79c0fc7ca08827
Nehal Patel <nepatel30@gmail.com>
2015-08-26 13:40:39 +1200

    all work

---
+++ www/app/apple-search.php
@@ -0,0 +1,31 @@
+<?php
+
+require '../../config.php';
+
+//get the search query
+
+$searchQuery = urlencode($_GET['searchQuery']);
+
+// start a connection using cURL
+$connection = curl_init();
+
+//Prepare option for the connection
+curl_setopt($connection, CURLOPT_URL, 'https://itunes.apple.com/search?term='.$searchQuery);
+curl_setopt($connection, CURLOPT_RETURNTRANSFER, true);
+
+
+//proxy settings
+curl_setopt($connection, CURLOPT_PROXY, 'proxy');
+curl_setopt($connection, CURLOPT_PROXYPORT, '3128');
+curl_setopt($connection, CURLOPT_PROXYUSERPWD, YOOBEE_LOGIN.':'.YOOBEE_PASSWORD);
+
+//Execute the connection
+$dataFromApple = curl_exec($connection);
+
+//Close the connection
+curl_close($connection);
+
+//Prepare the header
+header('Content-Type: application/json');
+
+echo $dataFromApple;
---
+++ www/js/appleSearch.js
@@ -0,0 +1,93 @@
+$(document).ready(function(){
+	//listen to button
+	$('#do-search').click(function(){
+
+		//get seach query value
+		var query = $('#query').val();
+
+		//Make sure there is a value
+		if ($.trim(query) == ''){
+
+			//dispaly error
+			console.log('blank query error');
+			return;
+		}
+
+
+		//ajax
+		$.ajax({
+
+			type:'get',
+			url:'app/apple-search.php',
+			data: {
+			searchQuery: query
+			},
+			beforeSend:function(){
+
+			},
+			success: function(dataFromServer){
+				console.log(dataFromServer);
+
+				$('#search-results').html('');//clear result
+
+				//loop over each item in the result set
+				$(dataFromServer.results).each(function(i){
+
+					//make life easier
+					var product = dataFromServer.results[i]; 	
+					//console.log(dataFromServer.results[i].artworkUrl100);
+
+					// $('#search-results').append('<img src="'+dataFromServer.results[i].artworkUrl100+'">');
+
+					//create di
+					var div = $('<div class="large-4 columns">');
+
+					// add the priview image to the div
+					var image = $('<img src="'+product.artworkUrl100+'">');
+
+					//console.log(image);
+
+					$(div).append(image);
+
+					//Create a preview element suitable for the priview file format
+					switch(product.kind){
+
+						case 'song':
+						case 'audiobook':
+						case 'music':
+
+							var preview = $('<audio controls preload="none" src="'+product.previewUrl+'">');
+
+						break;
+
+						case 'musicVideo':
+						case 'feature-movie':
+
+							var preview = $('<video controls preload="none" type="video/m4p" src="'+product.previewUrl+'">');
+							// var preview = $('<video controls preload="none"><source src="'+product.previewUrl+'"type="video/m4p">');
+
+						break;
+
+					}
+
+					//add the priview to the div
+					$(div).append(preview);
+
+					//add new product div to the page
+					$('#search-results').append(div);
+
+				});
+
+			},
+
+			error: function(){
+				console.log('sorry cannot connect to apple search api....');
+			}
+
+		});
+
+
+
+
+	});
+});
